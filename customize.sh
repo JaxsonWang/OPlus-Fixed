486f9f3a83eea13e208c9e4a2679294454eb101e
Jaxson Wang <i@iiong.com>
2025-02-12 10:37:16 +0800

    update

--- customize.sh
+++ customize.sh
@@ -12,13 +12,132 @@ REPLACE="
 /system/product/app/OplusSecurityKeyboard
 "
 
+chooseport() {
+    [ -n "$1" ] && local DELAY=$1 || local DELAY=10
+    [ "$2" = "YES" ] && local DEFAULT=$2 || local DEFAULT="NO"
+    local START_TIMESTAMP=$(date +%s)
+    local REMAINING_DELAY=$DELAY
+
+    ui_print "请在 $DELAY 秒内回答，否则将默认为 $DEFAULT"
+
+    while [ "$REMAINING_DELAY" -gt 0 ]; do
+        timeout ${REMAINING_DELAY}s /system/bin/getevent -lqc 1 2>&1 > $TMPDIR/events &
+        wait $! 2>/dev/null # 隐藏 "Terminated" 消息
+        if (`grep -q 'KEY_VOLUMEUP *DOWN' $TMPDIR/events`); then
+            ui_print "音量增加 (+) = 检测到 YES！"
+            return 0 # 0 = true
+        elif (`grep -q 'KEY_VOLUMEDOWN *DOWN' $TMPDIR/events`); then
+            ui_print "音量减少 (-) = 检测到 NO！"
+            return 1 # 1 = false
+        fi
+        REMAINING_DELAY=$(($START_TIMESTAMP + $DELAY - $(date +%s)))
+    done
+
+    ui_print "未检测到音量键操作..."
+    if [ "$DEFAULT" = "YES" ] ; then
+        ui_print "音量增加 (+) = 默认为 YES！"
+        return 0 # 0 = true
+    else
+        ui_print "音量减少 (-) = 默认为 NO！"
+        return 1 # 1 = false
+    fi
+}
+
+handleUnlockCNGms() {
+  if [ -e /system/etc/permissions/services.cn.google.xml ]; then
+      origin=/system/etc/permissions/services.cn.google.xml
+  elif [ -e /system/etc/permissions/com.oppo.features.cn_google.xml ]; then
+      origin=/system/etc/permissions/com.oppo.features.cn_google.xml
+  elif [ -e /vendor/etc/permissions/services.cn.google.xml ]; then
+      origin=/vendor/etc/permissions/services.cn.google.xml
+  elif [ -e /product/etc/permissions/services.cn.google.xml ]; then
+      origin=/product/etc/permissions/services.cn.google.xml
+  elif [ -e /product/etc/permissions/cn.google.services.xml ]; then
+      origin=/product/etc/permissions/cn.google.services.xml
+  elif [ -e /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml ]; then
+      origin=/my_bigball/etc/permissions/oplus_google_cn_gms_features.xml
+  elif [ -e /my_heytap/etc/permissions/my_heytap_cn_gms_features.xml ]; then
+      origin=/my_heytap/etc/permissions/my_heytap_cn_gms_features.xml
+  else
+      abort "File not found!"
+  fi
+
+  if [[ $origin == *my_bigball* ]]; then
+      target=$MODPATH/oplus_google_cn_gms_features.xml
+      echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/oplus_google_cn_gms_features.xml /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
+      # echo 'sleep 60; umount /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/service.sh
+  elif [[ $origin == *my_heytap* ]]; then
+      target=$MODPATH/my_heytap_cn_gms_features.xml
+      echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/my_heytap_cn_gms_features.xml /my_heytap/etc/permissions/my_heytap_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
+      if [[ -e /my_heytap/etc/permissions/my_heytap_cn_features.xml ]]; then
+          echo -e '\nmount -o ro,bind ${0%/*}/my_heytap_cn_features.xml /my_heytap/etc/permissions/my_heytap_cn_features.xml' >> $MODPATH/post-fs-data.sh
+          heytap_cn_features_orgin=/my_heytap/etc/permissions/my_heytap_cn_features.xml
+          heytap_cn_features_target=$MODPATH/my_heytap_cn_features.xml
+      fi
+  elif [[ $origin == *system* || $KSU ]]; then
+      target=$MODPATH$origin    
+  else
+      target=$MODPATH/system$origin
+  fi
+
+  mkdir -p $(dirname $target)
+  cp -f $origin $target
+  sed -i '/cn.google.services/d' $target
+  sed -i '/services_updater/d' $target
+  ui_print "modify $origin"
+
+  if [[ -e $heytap_cn_features_orgin ]]; then
+  mkdir -p $(dirname $heytap_cn_features_target)
+  cp -f $heytap_cn_features_orgin $heytap_cn_features_target
+  sed -i '/cn.google.services/d' $heytap_cn_features_target
+  sed -i '/services_updater/d' $heytap_cn_features_target
+  ui_print "modify $heytap_cn_features_orgin"
+  fi
+}
+
+ui_print ""
+ui_print "+ 模块个性化配置"
+ui_print ""
+ui_print "*** 请使用音量按键选择 ***"
+ui_print "*** 音量上键 (+) = 是 ***"
+ui_print "*** 音量上键 (-) = 否 ***"
+
+ui_print ""
+ui_print "*** 是否安装 Android Auto 模块 ***"
+ui_print ""
+if chooseport 20 "NO"; then # 0 = True, why shell why..
+    ui_print "Android Auto 模块已添加安装！"
+else
+    rm -r "$MODPATH/system/product/app/GoogleTTS";
+    rm -r "$MODPATH/system/product/app/Maps";
+    rm -r "$MODPATH/system/product/etc/permissions/com.google.android.projection.gearhead.xml";
+    rm -r "$MODPATH/system/product/overlay/AndroidAutoOverlay";
+    rm -r "$MODPATH/system/product/priv-app/AndroidAutoStubPrebuilt";
+    rm -r "$MODPATH/system/product/priv-app/Velvet";
+    ui_print "Android Auto 模块已忽略"！
+fi
+
+ui_print ""
+ui_print "*** 是否安装 Unlock CN GMS 模块 ***"
+ui_print ""
+if chooseport 20 "NO"; then # 0 = True, why shell why..
+    handleUnlockCNGms
+    ui_print "Unlock CN GMS 模块已添加安装！"
+else
+    ui_print "Android Auto 模块已忽略"！
+fi
+
+ui_print ""
 if [ -d /data/adb/modules/tricky_store ]; then
+  ui_print "- keybox 安装成功！"
   mkdir -p /data/adb/tricky_store
   mv "$MODPATH/keybox.xml" /data/adb/tricky_store
 else
-  ui_print "- TrickyStore module not installed!"
+  ui_print "- TrickyStore 模块没有安装！"
 fi
 
 chcon -r u:object_r:system_file:s0 "$MODPATH/system"
 set_perm_recursive $MODPATH  0  0  0755  0644
 set_perm_recursive $MODPATH/system/bin  0  2000  0755 0755 u:object_r:same_process_hal_file:s0
+
+ui_print ""
