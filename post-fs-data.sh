a217f5892d1875c12d5f15cc6c1822c110a41f6f
Jaxson Wang <i@iiong.com>
2025-06-17 08:58:26 +0800

    update

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,9 +1,6 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-mount --bind $MODDIR/my_product/etc/extension/com.oplus.app-features.xml /my_product/etc/extension/com.oplus.app-features.xml
-mount --bind $MODDIR/my_product/etc/extension/com.oplus.oplus-feature.xml /my_product/etc/extension/com.oplus.oplus-feature.xml
-
 # Google CN GMS Features
 mount --bind $MODDIR/my_product/etc/permissions/oplus_google_cn_gms_features.xml /my_product/etc/permissions/oplus_google_cn_gms_features.xml
 mount --bind $MODDIR/my_product/etc/permissions/oplus.feature.control_cn_gms.xml /my_product/etc/permissions/oplus.feature.control_cn_gms.xml
