b6ae9379897b60757a817526e6701540a5291b3f
Jaxson Wang <i@iiong.com>
2026-01-30 14:55:10 +0800

    update

--- action.sh
+++ action.sh
@@ -3,14 +3,9 @@
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 MODDIR="${0%/*}"
 
-chmod 755 "$MODDIR/key-box.sh"
 chmod 755 "$MODDIR/soter-fixed.sh"
 chmod 755 "$MODDIR/clear-fixed.sh"
 chmod 755 "$MODDIR/prop.sh"
-
-. "$MODDIR/key-box.sh"
-
-provision_keybox
 
 sh "$MODDIR/clear-fixed.sh"
 sh "$MODDIR/prop.sh"
--- service.sh
+++ service.sh
@@ -2,21 +2,9 @@
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 MODDIR="${0%/*}"
 
-chmod 755 "$MODDIR/key-box.sh"
 chmod 755 "$MODDIR/soter-fixed.sh"
 chmod 755 "$MODDIR/clear-fixed.sh"
 chmod 755 "$MODDIR/prop.sh"
-
-. "$MODDIR/key-box.sh"
-
-copy_library() {
-    cp /vendor/lib64/libminkdescriptor.so "$MODDIR/"
-    chmod 644 "$MODDIR/libminkdescriptor.so"
-    log "Copied libminkdescriptor.so to module directory"
-}
-
-copy_library
-provision_keybox
 
 sh "$MODDIR/clear-fixed.sh"
 sh "$MODDIR/prop.sh"
